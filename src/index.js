338c77e16022fd318cc94ce714af236c347d62e2
jcornelio <jcornelio@lordabbett.com>
2018-07-27 13:59:16 -0400

    tree shaking - add utility

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import './style.css';
- import _ from 'lodash';
+import { cube } from './math.js';
  import printMe from './print.js';
 
 function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+  var element = document.createElement('pre');
+ 
+  element.innerHTML = [
+     'Hello webpack!',
+     '5 cubed is equal to ' + cube(5)
+   ].join('\n\n');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-     btn.innerHTML = 'Click me and check the console!';
-   btn.onclick = printMe;
-
-   element.appendChild(btn);
-
+ 
   return element;
 }
 
 document.body.appendChild(component());
+
+ if (module.hot) {
+   module.hot.accept('./print.js', function() {
+     console.log('Accepting the updated printMe module!');
+     printMe();
+   })
+ }
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
   module.exports = {
    entry: {
-     app: './src/index.js',
-     print: './src/print.js'
+   	app: './src/index.js'
    },
     
     devtool: 'inline-source-map',
 
     devServer: {
-     contentBase: './dist'
+     contentBase: './dist',
+     hot: true
    },
 
    plugins: [
@@ -19,13 +20,17 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
      new HtmlWebpackPlugin({
        title: 'Output Management'
-     })
+     }),
+
+     new webpack.HotModuleReplacementPlugin()
    ],
 
  output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist')
     },
+ 	
+ 	mode: "development",
 
    	module: {
      rules: [
