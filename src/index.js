0454a4c10807893cbe2878478a022a804a7e7ff7
jcornelio <jcornelio@lordabbett.com>
2018-07-27 11:12:46 -0400

    first commit - css

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+ import _ from 'lodash';
+ 
+function component() {
+  var element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
